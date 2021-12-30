2dbbaae048010e480a8fb5079646baa928efd782
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-30 22:47:57 +0530

    ADDED: debit method

--- src/com/bridgelabz/oops/Stock.java
+++ src/com/bridgelabz/oops/Stock.java
@@ -1,16 +1,30 @@
 package com.bridgelabz.oops;
 
 public class Stock {
-     private String shareName;
-   private int noOfShare;
-     private int sharePrice;
+    private String shareName;
+    private int noOfShare;
+    private int sharePrice;
     public Stock(String shareName, int noOfShare, int sharePrice) {
         super();
         this.shareName = shareName;
         this.noOfShare = noOfShare;
         this.sharePrice = sharePrice;
     }
-    	@Override
+
+    public String getShareName() {
+        return shareName;
+    }
+
+    public int getNoOfShare() {
+        return noOfShare;
+    }
+
+    public int getSharePrice() {
+        return sharePrice;
+    }
+
+
+    @Override
     public String toString() {
         return "Stock [shareName=" +shareName + ", noOfShare=" + noOfShare + ", sharePrice=" + sharePrice + "]";
     }
